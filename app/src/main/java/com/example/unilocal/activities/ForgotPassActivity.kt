af274cf3f655d6b97ac04f6e2c1c5afe29abe312
ElSantiagoBernal <bernal.santiago.8994@eam.edu.co>
2023-04-09 16:33:40 -0500

    Más cambios

--- app/src/main/java/com/example/unilocal/activities/ForgotPassActivity.kt
+++ app/src/main/java/com/example/unilocal/activities/ForgotPassActivity.kt
@@ -3,10 +3,16 @@ package com.example.unilocal.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.unilocal.R
+import com.example.unilocal.databinding.ActivityForgotPassBinding
+import com.example.unilocal.databinding.ActivityRegisterBinding
 
 class ForgotPassActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityForgotPassBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_pass)
+        binding = ActivityForgotPassBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
